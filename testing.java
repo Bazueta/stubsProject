db4e13a08d290f27677c41078c935525f4e6fb80
Anna Bortle <abortle@udel.edu>
2019-04-08 21:40:33 -0400

    adding new tests

--- testing.java
+++ testing.java
@@ -1,10 +1,37 @@
 package project;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class testing {
-
+	
+	//Osprey Class
+	@Test
+	public void OspreyMoveTest1() {
+		Osprey o1 = new Osprey(0,0,1,1);
+		o1.move();
+		assertEquals(1,o1.xloc);
+		assertEquals(1,o1.yloc);
+	}
+	@Test
+	public void OspreyMoveTest2() {
+		Osprey o2 = new Osprey(50,20,5,-1); 
+		o2.move();
+		o2.move();
+		assertEquals(60,o2.xloc);
+		assertEquals(18,o2.yloc);
+	}
+	@Test
+	public void OspreyMoveTest3() {
+		Osprey o3 = new Osprey(10,30,30,10);
+		o3.move();
+		o3.move();
+		o3.move();
+		assertEquals(100, o3.xloc);
+		assertEquals(60, o3.yloc);
+	}
+	
+	//Scoring Class
 	@Test
 	public void ScoringTest() {
 		Scoring scoring = new Scoring();
@@ -16,8 +43,4 @@ public class testing {
 		//assertEquals(scoring.ScoringTable.get("3"),2);
 	}
 	
-	@Test
-	public void ScoringObjectTest() {
-		
-	}
 }
