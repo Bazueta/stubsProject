dbb5ab11ce94fa918b4a8061dd4c0aab3e2b5888
hannahbridge <36232373+hannahbridge@users.noreply.github.com>
2019-04-08 22:37:30 -0400

    clapper rail

--- ClapperRail.java
+++ ClapperRail.java
@@ -6,14 +6,35 @@ public class ClapperRail {
 	
 	int xloc;
 	int yloc;
-	int xincr;
-	int yincr;
+	int xIncr;
+	int yIncr;
 	BufferedImage[][] imageArray;
 	
+	ClapperRail(int xloc, int yloc, int xIncr, int yIncr){
+		this.xloc = xloc;
+		this.yloc = yloc;
+		this.xIncr = xIncr;
+		this.yIncr = yIncr;
+	}
+	
+	public int getxloc(){
+		return this.xloc;
+	}
+	
+	public int getyloc(){
+		return this.yloc;
+	}
+	
+	/**
+	 * updates x location and y location for clapper rail object
+	 * @param none
+	 * @return none
+	 * @author Hannah Bridge
+	 */
 	public void move() {
 		System.out.println("player move method called.");
 	
-		xloc += xincr;
-		yloc += yincr;
+		this.xloc += this.xIncr;
+		this.yloc += this.yIncr;
 	}
 }
